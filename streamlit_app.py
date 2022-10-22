6c00274972a98d6d97dcd49d947e06e2d2df91db
SARAVANAN DURAIRAJAN <116052816+Durairs@users.noreply.github.com>
2022-10-22 18:49:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,5 +38,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # coverted the normailized version in a dataframe.
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 
 
