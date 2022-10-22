c9df2b51659ffeb3e62bc35b4c93922d995bb926
SARAVANAN DURAIRAJAN <116052816+Durairs@users.noreply.github.com>
2022-10-22 16:38:28 -0500

    Update streamlit_app.py
    get the json and normalized into a dataframe.
    first step in passing parameter to api

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,13 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+#take the json version and normalize it
+# store the normalized version in a variable 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# coverted the normailized version in a dataframe.
+streamlit.dataframe(fruityvice_normalized)
+
 
 
