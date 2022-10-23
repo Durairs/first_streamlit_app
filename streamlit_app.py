da6ad3f139f0250627ef0a40cc73fd1073e66813
SARAVANAN DURAIRAJAN <116052816+Durairs@users.noreply.github.com>
2022-10-22 19:26:09 -0500

    Update streamlit_app.py
    added insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-user_fruit_add = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding', user_fruit_add)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding', add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
