e524f3fdcb6d6366e8cedcd6054c52d02a956769
SARAVANAN DURAIRAJAN <116052816+Durairs@users.noreply.github.com>
2022-10-22 19:54:35 -0500

    Update streamlit_app.py
    indentation reset

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,13 +34,13 @@ def get_fruitvice_data(this_fruit_choice):
 #API Calls
 # New Section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-          streamlit.error('Please select a fruit to get information.')
-    else:
-          back_from_function = get_fruityvice_data(fruit_choice)
-          streamlit.dataframe(back_from_function)
+        try:
+                fruit_choice = streamlit.text_input('What fruit would you like information about?')
+                if not fruit_choice:
+                        streamlit.error('Please select a fruit to get information.')
+                else:
+                        back_from_function = get_fruityvice_data(fruit_choice)
+                        streamlit.dataframe(back_from_function)
             
 #except URLError as e:
 #          streamlit.error()
